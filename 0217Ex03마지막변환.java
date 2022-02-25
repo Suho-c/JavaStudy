a2093a3fc01ac25a3e9572aa8db868aa25d7dd3a
SuhoChoi <93629667+choisuho0408@users.noreply.github.com>
2022-02-25 21:02:14 +0900

    Rename Ex03마지막변환.java to 0217Ex03마지막변환.java

---
+++ 0217Ex03마지막변환.java
@@ -0,0 +1,20 @@
+
+public class Ex03마지막변환 {
+
+	public static void main(String[] args) {
+
+		// int 형 변수 num에 777을 넣고 생성
+		int num = 777;
+		int result = num / 10 * 10 + 1;
+		// 결과에는 771출력되게 만들기!
+		// ex) num = 235; 를 넣을때는 결과가 231로 출력
+		
+		System.out.println("결과는 : " + result);		
+		
+		
+		
+		
+
+	}
+
+}
