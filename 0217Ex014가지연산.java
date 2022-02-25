e05f8299577237bfdda830ba46b3e18ed613c625
SuhoChoi <93629667+choisuho0408@users.noreply.github.com>
2022-02-25 21:00:48 +0900

    Rename Ex014가지연산.java to 0217Ex014가지연산.java

---
+++ 0217Ex014가지연산.java
@@ -0,0 +1,22 @@
+
+public class Ex014가지연산 {
+	
+	public static void main(String[] args) {
+		int num1 = 7;
+		int num2 = 3;
+		
+		int sum = num1 + num2;
+		int minus = num1 - num2;
+		int mul = num1 * num2;
+		double div = (double)num1 / num2;
+		
+		// num1 과 num2 더하기, 빼기, 곱하기, 나누기 결과 출력
+		System.out.println("더하기 결과 : " + sum);
+		System.out.println("빼기 결과 : " + minus);
+		System.out.println("곱하기 결과 : " + mul);
+		System.out.println("나누기 결과 : " + div);
+		
+				
+		
+	}
+}
