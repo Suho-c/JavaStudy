4d59cdd2ee709c60b7e8fd229323b2a54fa4c32e
SuhoChoi <93629667+choisuho0408@users.noreply.github.com>
2022-02-27 19:49:08 +0900

    Update and rename Ex01_구구단.java to 0226Ex01_구구단.java

--- 0226Ex01_구구단.java
+++ 0226Ex01_구구단.java
@@ -1,10 +1,10 @@
 
-public class Ex01_±¸±¸´Ü {
+public class Ex01_êµ¬êµ¬ë‹¨ {
 
 	public static void main(String[] args) {
 		
 		for(int i = 2; i < 10; i++) {
-			System.out.print(i + "´Ü" + "\n");
+			System.out.print(i + "Â´Ãœ" + "\n");
 			for(int j = 1; j < 10; i ++) {
 				System.out.print(i + " * " + j + " = " + i * j);
 			}
