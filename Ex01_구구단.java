476f524ffbb003cc9ddaa91aba15e6f6d387d154
SuhoChoi <93629667+choisuho0408@users.noreply.github.com>
2022-02-27 19:48:28 +0900

    Add files via upload

---
+++ Ex01_구구단.java
@@ -0,0 +1,14 @@
+
+public class Ex01_구구단 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 2; i < 10; i++) {
+			System.out.print(i + "단" + "\n");
+			for(int j = 1; j < 10; i ++) {
+				System.out.print(i + " * " + j + " = " + i * j);
+			}
+		}
+	}
+
+}
---
+++ Ex04_별찍기2.java
@@ -0,0 +1,19 @@
+
+public class Ex04_º°Âï±â2 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i < 6; i++) {
+			for(int j = 1; j>= i; j--) {
+				if(i>j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
